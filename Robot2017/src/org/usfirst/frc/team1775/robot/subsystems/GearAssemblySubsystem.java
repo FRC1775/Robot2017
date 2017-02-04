c53fb81e78f3d41e4f80cfce7226b6199d3db2c6
tigerbytes1775 <tigerbytes1775@gmail.com>
2017-02-04 11:15:06 -0600

    Add stub subsystems

---
+++ Robot2017/src/org/usfirst/frc/team1775/robot/subsystems/GearAssemblySubsystem.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team1775.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class GearAssemblySubsystem extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Robot2017/src/org/usfirst/frc/team1775/robot/subsystems/WinchSubsystem.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team1775.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class WinchSubsystem extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
