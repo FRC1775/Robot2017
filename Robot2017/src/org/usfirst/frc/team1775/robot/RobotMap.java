36c5402e96242a7ea396a8ddc747446b0e4a5386
tigerbytes1775 <tigerbytes1775@gmail.com>
2017-02-25 16:28:02 -0600

    Clean up robotmap

--- Robot2017/src/org/usfirst/frc/team1775/robot/RobotMap.java
+++ Robot2017/src/org/usfirst/frc/team1775/robot/RobotMap.java
@@ -12,13 +12,6 @@ import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import com.ctre.CANTalon;
-import com.ctre.CANTalon.FeedbackDevice;
-import com.ctre.CANTalon.TalonControlMode;
-import com.ctre.CANTalon.VelocityMeasurementPeriod;
-
-import edu.wpi.first.wpilibj.Encoder;
-
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -26,6 +19,49 @@ import edu.wpi.first.wpilibj.Encoder;
  * floating around.
  */
 public class RobotMap {
+	
+	public final static boolean IS_COMPETITION_BOT = false;
+	
+	/*
+	 * 
+	 * Competition Robot Channel Configuration
+	 * 
+	 */
+	
+	public final static int COMPETITION_DRIVE_TRAIN_LEFT_CONTROLLER_PWM_CHANNEL = 2;
+	public final static int COMPETITION_DRIVE_TRAIN_RIGHT_CONTROLLER_PWM_CHANNEL = 1;
+	public final static int COMPETITION_DRIVE_TRAIN_ENCODER_DIO_CHANNEL_A = 2;
+	public final static int COMPETITION_DRIVE_TRAIN_ENCODER_DIO_CHANNEL_B = 3;
+	
+	public final static int COMPETITION_SHOOTER_SINGULATOR_CONTROLLER_PWM_CHANNEL = 0;
+	public final static int COMPETITION_SHOOTER_REGULATOR_CONTROLLER_PWM_CHANNEL = 3;
+	public final static int COMPETITION_SHOOTER_CONTROLLER_PWM_CHANNEL = 6;
+	public final static int COMPETITION_SHOOTER_ENCODER_DIO_CHANNEL_A = 4;
+	public final static int COMPETITION_SHOOTER_ENCODER_DIO_CHANNEL_B = 5;
+	
+	public final static int COMPETITION_GEAR_RELEASE_PCM_CHANNEL = 0;
+	public final static int COMPETITION_WINCH_CONTROLLER_PWM_CHANNEL = 4;
+	
+	/*
+	 * 
+	 * Practice Robot Channel Configuration
+	 * 
+	 */
+	
+	public final static int PRACTICE_DRIVE_TRAIN_LEFT_CONTROLLER_PWM_CHANNEL = 2;
+	public final static int PRACTICE_DRIVE_TRAIN_RIGHT_CONTROLLER_PWM_CHANNEL = 1;
+	public final static int PRACTICE_DRIVE_TRAIN_ENCODER_DIO_CHANNEL_A = 2;
+	public final static int PRACTICE_DRIVE_TRAIN_ENCODER_DIO_CHANNEL_B = 3;
+	
+	public final static int PRACTICE_SHOOTER_SINGULATOR_CONTROLLER_PWM_CHANNEL = 0;
+	public final static int PRACTICE_SHOOTER_REGULATOR_CONTROLLER_PWM_CHANNEL = 3;
+	public final static int PRACTICE_SHOOTER_CONTROLLER_PWM_CHANNEL = 6;
+	public final static int PRACTICE_SHOOTER_ENCODER_DIO_CHANNEL_A = 4;
+	public final static int PRACTICE_SHOOTER_ENCODER_DIO_CHANNEL_B = 5;
+	
+	public final static int PRACTICE_GEAR_RELEASE_PCM_CHANNEL = 0;
+	public final static int PRACTICE_WINCH_CONTROLLER_PWM_CHANNEL = 4;
+	
 	// Gyro
 	public static Gyro gyro;
 	
@@ -39,22 +75,18 @@ public class RobotMap {
 	public static SpeedController shooterSingulatorController;
 	public static SpeedController shooterRegulatorController;
 	public static SpeedController shooterController;
-	//public static CANTalon shooterController;
 	public static Encoder shooterEncoder;
+	//public static CANTalon shooterController;
+	
 	// Gear Assembly
 	public static Solenoid gearRelease;
-	public static SpeedController gearRotatorController;
-	public static DigitalInput gearSpokeDetectorOne;
-	public static DigitalInput gearSpokeDetectorTwo;
+	// public static SpeedController gearRotatorController;
+	// public static DigitalInput gearSpokeDetectorOne;
+	// public static DigitalInput gearSpokeDetectorTwo;
 	
 	// Winch
 	public static SpeedController winchController;
 	
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
 	public static void init() {
 		initGyro();
 		initDriveTrain();
@@ -67,52 +99,52 @@ public class RobotMap {
 		gyro = new ADXRS450_Gyro();
 		gyro.calibrate();
 	}
-	
-	private static void initDriveTrain() {
-		driveTrainLeftController = new Talon(2);
-		LiveWindow.addActuator("DriveTrain", "LeftController", (Talon) driveTrainLeftController);
 
-		driveTrainRightController = new Talon(1);
-		LiveWindow.addActuator("DriveTrain", "RightController", (Talon) driveTrainRightController);
+	private static void initDriveTrain() {
+		driveTrainLeftController = new Talon(getDriveTrainLeftControllerPwmChannel());
+		LiveWindow.addActuator("Drive Train", "LeftController", (Talon) driveTrainLeftController);
+
+		driveTrainRightController = new Talon(getDriveTrainRightControllerPwmChannel());
+		LiveWindow.addActuator("Drive Train", "RightController", (Talon) driveTrainRightController);
 
 		driveTrainRobotDrive = new RobotDrive(driveTrainLeftController, driveTrainRightController);
 
 		driveTrainRobotDrive.setSafetyEnabled(true);
 		driveTrainRobotDrive.setExpiration(0.1);
 		driveTrainRobotDrive.setSensitivity(0.5);
-		driveTrainRobotDrive.setMaxOutput(getMaxOutput());
+		driveTrainRobotDrive.setMaxOutput(1);
 		driveTrainRobotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
 		driveTrainRobotDrive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
 		
-		// TODO drive train encoder
-		driveTrainEncoder = new Encoder(2, 3, false, Encoder.EncodingType.k4X);
+		// Drive train encoder
+		driveTrainEncoder = new Encoder(getDriveTrainEncoderDioChannelA(), getDriveTrainEncoderDioChannelB(), false, Encoder.EncodingType.k4X);
 		
 		//driveTrainEncoder.setDistancePerPulse(10);
 		//Quadrature 4x
 		double distancePerPulse = ((3.5*Math.PI)/250.0)*1.067; // distance in inches multiplied by ratio error factor
 		//if because this is a 4x encoder that I need to divide by 1000 or 250
 		driveTrainEncoder.setDistancePerPulse(distancePerPulse);
-		double encoderValue = driveTrainEncoder.getDistance();
+		//double encoderValue = driveTrainEncoder.getDistance();
 		//SmartDashboard.putNumber("DriveTrainEncoder", encoderValue );
-		
-		
 	}
 	
 	private static void initShooter() {
-		shooterSingulatorController = new Talon(0);
+		shooterSingulatorController = new Talon(getShooterSingulatorControllerPwmChannel());
 		LiveWindow.addActuator("Shooter", "SingulatorController", (Talon) shooterSingulatorController);
 		
-		shooterRegulatorController = new Talon(3);
+		shooterRegulatorController = new Talon(getShooterRegulatorControllerPwmChannel());
 		LiveWindow.addActuator("Shooter", "RegulatorController", (Talon) shooterRegulatorController);
 		
-		shooterEncoder = new Encoder(4, 5, false, Encoder.EncodingType.k1X);
+		shooterController = new Talon(getShooterControllerPwmChannel());
+		LiveWindow.addActuator("Shooter", "Controller", (Talon) shooterController);
+		shooterController.setInverted(true);
+		
+		shooterEncoder = new Encoder(getShooterEncoderDioChannelA(), getShooterEncoderDioChannelB(), false, Encoder.EncodingType.k1X);
 		shooterEncoder.setDistancePerPulse(360 / 20);
 		shooterEncoder.setSamplesToAverage(10);
 		
-		shooterController = new Talon(6);
-		shooterController.setInverted(true);
-		
 		/*
+		 * Talon SRX which we aren't using now.
 		shooterController = new CANTalon(0);
 		shooterController.reverseSensor(false);
 		shooterController.reverseOutput(true);
@@ -136,39 +168,104 @@ public class RobotMap {
 	}
 	
 	private static void initGearAssembly() {
-		// TODO initialize gear components
-
-		
-		gearRelease = new Solenoid( 0 ) ;
-		//gearRelease.set(true);
-		//gearRelease.set(false);
-		/*
-		gearRotatorController = new Talon( 5 ) ;
-		
-		gearSpokeDetectorOne = new DigitalInput(   ) ;
-		gearSpokeDetectorTwo = new DigitalInput(   ) ;
-		
-		//public static SpeedController gearRotatorController;
-		//public static DigitalInput gearSpokeDetectorOne;
-		//public static DigitalInput gearSpokeDetectorTwo;
-		*/
+		gearRelease = new Solenoid(getGearReleasePcmChannel());
+		LiveWindow.addActuator("Gear Assembly", "Release", gearRelease);
 	}
 	
 	private static void initWinch() {
-		winchController = new Talon(4);
-		LiveWindow.addActuator("Winch", "WinchController", (Talon) winchController);
+		winchController = new Talon(getWinchControllerPwmChannel());
+		LiveWindow.addActuator("Winch", "Controller", (Talon) winchController);
 	}
 	
+	private static int getDriveTrainLeftControllerPwmChannel() {
+		if (isCompetitionBot()) {
+			return COMPETITION_DRIVE_TRAIN_LEFT_CONTROLLER_PWM_CHANNEL;
+		} else {
+			return PRACTICE_DRIVE_TRAIN_LEFT_CONTROLLER_PWM_CHANNEL;
+		}
+	}
+	
+	private static int getDriveTrainRightControllerPwmChannel() {
+		if (isCompetitionBot()) {
+			return COMPETITION_DRIVE_TRAIN_RIGHT_CONTROLLER_PWM_CHANNEL;
+		} else {
+			return PRACTICE_DRIVE_TRAIN_RIGHT_CONTROLLER_PWM_CHANNEL;
+		}
+	}
+	
+	private static int getDriveTrainEncoderDioChannelA() {
+		if (isCompetitionBot()) {
+			return COMPETITION_DRIVE_TRAIN_ENCODER_DIO_CHANNEL_A;
+		} else {
+			return PRACTICE_DRIVE_TRAIN_ENCODER_DIO_CHANNEL_A;
+		}
+	}
+	
+	private static int getDriveTrainEncoderDioChannelB() {
+		if (isCompetitionBot()) {
+			return COMPETITION_DRIVE_TRAIN_ENCODER_DIO_CHANNEL_B;
+		} else {
+			return PRACTICE_DRIVE_TRAIN_ENCODER_DIO_CHANNEL_B;
+		}
+	}
+	
+	private static int getShooterSingulatorControllerPwmChannel() {
+		if (isCompetitionBot()) {
+			return COMPETITION_SHOOTER_SINGULATOR_CONTROLLER_PWM_CHANNEL;
+		} else {
+			return PRACTICE_SHOOTER_SINGULATOR_CONTROLLER_PWM_CHANNEL;
+		}
+	}
+	
+	private static int getShooterRegulatorControllerPwmChannel() {
+		if (isCompetitionBot()) {
+			return COMPETITION_SHOOTER_REGULATOR_CONTROLLER_PWM_CHANNEL;
+		} else {
+			return PRACTICE_SHOOTER_REGULATOR_CONTROLLER_PWM_CHANNEL;
+		}
+	}
+	
+	private static int getShooterControllerPwmChannel() {
+		if (isCompetitionBot()) {
+			return COMPETITION_SHOOTER_CONTROLLER_PWM_CHANNEL;
+		} else {
+			return PRACTICE_SHOOTER_CONTROLLER_PWM_CHANNEL;
+		}
+	}
+	
+	private static int getShooterEncoderDioChannelA() {
+		if (isCompetitionBot()) {
+			return COMPETITION_SHOOTER_ENCODER_DIO_CHANNEL_A;
+		} else {
+			return PRACTICE_SHOOTER_ENCODER_DIO_CHANNEL_A;
+		}
+	}
+	
+	private static int getShooterEncoderDioChannelB() {
+		if (isCompetitionBot()) {
+			return COMPETITION_SHOOTER_ENCODER_DIO_CHANNEL_B;
+		} else {
+			return PRACTICE_SHOOTER_ENCODER_DIO_CHANNEL_B;
+		}
+	}
+	
+	private static int getGearReleasePcmChannel() {
+		if (isCompetitionBot()) {
+			return COMPETITION_GEAR_RELEASE_PCM_CHANNEL;
+		} else {
+			return PRACTICE_GEAR_RELEASE_PCM_CHANNEL;
+		}
+	}
 
-	private static double getMaxOutput() {
-		double maxOutput = Preferences.getInstance().getDouble("DriveTrain.maxOutput", 1.0);
-
-		if (maxOutput > 1)
-			maxOutput = 1.0;
-		if (maxOutput < 0)
-			maxOutput = 0;
-		SmartDashboard.putNumber("DriveTrain.maxOutput", maxOutput);
-
-		return maxOutput;
+	private static int getWinchControllerPwmChannel() {
+		if (isCompetitionBot()) {
+			return COMPETITION_WINCH_CONTROLLER_PWM_CHANNEL;
+		} else {
+			return PRACTICE_WINCH_CONTROLLER_PWM_CHANNEL;
+		}
+	}
+	
+	private static boolean isCompetitionBot() {
+		return IS_COMPETITION_BOT;
 	}
 }
