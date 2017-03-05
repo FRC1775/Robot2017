7853501882c95161fcd3ca5cbbd378cc248444d9
tigerbytes1775 <tigerbytes1775@gmail.com>
2017-03-04 22:38:26 -0600

    Updates to gear assembly

--- Robot2017/src/org/usfirst/frc/team1775/robot/commands/gearassembly/ReleaseGear.java
+++ Robot2017/src/org/usfirst/frc/team1775/robot/commands/gearassembly/ReleaseGear.java
@@ -9,8 +9,16 @@ public class ReleaseGear extends Command {
 	
 	public static GearAssemblySubsystem gearAssembly = Robot.gearAssembly;
 	
+	private boolean keepOpen = false;
+	
 	public ReleaseGear() {
-		requires(Robot.gearAssembly);
+		requires(gearAssembly);
+	}
+	
+	public ReleaseGear(boolean keepOpen) {
+		this();
+		
+		this.keepOpen = keepOpen;
 	}
 	
 	@Override
@@ -18,22 +26,15 @@ public class ReleaseGear extends Command {
 		gearAssembly.release();
 	}
 
-	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
 		return false;
 	}
 
-	// Called once after isFinished returns true
-	@Override
-	protected void end() {
-	}
-
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
 	@Override
 	protected void interrupted() {
-		System.out.println("HEREHERE");
-		gearAssembly.reset();
+		if (!keepOpen) {
+			gearAssembly.reset();
+		}
 	}
 }
--- Robot2017/src/org/usfirst/frc/team1775/robot/subsystems/GearAssemblySubsystem.java
+++ Robot2017/src/org/usfirst/frc/team1775/robot/subsystems/GearAssemblySubsystem.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team1775.robot.subsystems;
 import org.usfirst.frc.team1775.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class GearAssemblySubsystem extends Subsystem {
 
@@ -10,10 +11,12 @@ public class GearAssemblySubsystem extends Subsystem {
 	protected void initDefaultCommand() { }
 	
 	public void release() {
+		SmartDashboard.putBoolean("Gear.isOpen", true);
 		RobotMap.gearRelease.set(true);
 	}
 	
 	public void reset() {
+		SmartDashboard.putBoolean("Gear.isOpen", false);
 		RobotMap.gearRelease.set(false);
 	}
 
